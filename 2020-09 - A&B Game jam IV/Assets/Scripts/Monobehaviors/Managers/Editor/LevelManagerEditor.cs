eaf6950e9e43e07af406f9a1a41f40ee1e04a99f
Nefisto <joao.gavron@gmail.com>
2020-09-03 19:52:46 -0300

    .

---
+++ 2020-09 - A&B Game jam IV/Assets/Scripts/Monobehaviors/Managers/Editor/LevelManagerEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelManager))]
+public class LevelManagerEditor : Editor
+{
+    public LevelManager _target
+    {
+        get => (LevelManager)target;
+    }
+    
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Start stage"))
+            _target.StartLevel();
+    }
+}
